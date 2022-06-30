4ebef1f1b821940c68811f666b437637d0ec3211
Captain Levi <sharishthsingh@gmail.com>
2022-06-30 14:04:01 +0530

    30 June

---
+++ Coin_Piles.cpp
@@ -0,0 +1,91 @@
+/*
+    _________________________________________
+   /                                         \
+   | ; Title: cp template                    |
+   | ; Author:  Sharishth Singh              |
+   | ; Date: 22 May 2022                     |
+   \                                         /
+    -----------------------------------------
+         \  ^__^
+          \ (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+*/
+
+#include "bits/stdc++.h"
+#include <algorithm>
+#include <climits>
+#include <shared_mutex>
+#include <vector>
+using namespace std;
+#define parr(x)                                                                 \
+   cerr << #x << ' ' << '[' << ' ';                                             \
+   for (auto z : x)                                                             \
+      cerr << z << ' ';                                                         \
+   cerr << ']' << endl
+#define pmap(x)                                                                 \
+   cerr << #x << ' ' << '{' << ' ';                                             \
+   for (auto z : x)                                                             \
+      cerr << '(' << ' ' << z.first << ' ' << z.second << ' ' << ')' << ' ';    \
+   cerr << '}' << endl
+#define fast                                                                    \
+   ios_base::sync_with_stdio(false);                                            \
+   cin.tie(NULL);                                                               \
+   cout.tie(NULL);                                                              \
+   cerr.tie(NULL);
+#define bug(x)                                                                  \
+   cerr << #x << " : ";                                                         \
+   cerr << x << endl
+#define ff first
+#define ss second
+#undef int
+#define int unsigned long long int
+#define endl '\n'
+#define pi 3.1415926535
+#define ve vector
+int **memorized_dp_array;
+int no_of_vertix = 0;
+int **adj_matrix;
+int MOD = 1e9 + 7;
+std::map<int, vector<int>> tree;
+int *vis;
+char **grid;
+/* int start; */
+int *path;
+int ele=0;
+
+int n;
+int n_;
+int *arr;
+
+void solution() {
+   int a,b;
+   cin >> a >> b;
+   if(b > a){
+      swap(a, b);
+   }
+   if(2*b < a){
+      std::cout << "NO" << endl;
+      return;
+   }
+   if((b+a)%3 == 0){
+      std::cout << "YES" << endl;
+   }
+   else{
+      std::cout << "NO" << endl;
+   }
+}
+
+int32_t main() {
+   fast;
+   int test_cases = 1;
+   cin >> test_cases;
+   /* int a = 1; */
+   while (test_cases--) {
+      /* std::cout << "Case #"<< a << ": "; */
+      solution();
+      /* a++; */
+   }
+}
+
