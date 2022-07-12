a5eb5390ffc5afa2fc535435c08b2706ac3b221c
Captain Levi <sharishthsingh@gmail.com>
2022-07-12 14:22:53 +0530

    12 July

---
+++ Creating_Strings.cpp
@@ -0,0 +1,86 @@
+/*
+    _________________________________________
+   /                                         \
+   | ; Title: cp template                    |
+   | ; Author:  Sharishth Singh              |
+   | ; Date: 22 May 2022                     |
+   \                                         /
+    -----------------------------------------
+      \  ^__^
+       \ (oo)\_______
+         (__)\       )\/\
+             ||----w |
+             ||     ||
+*/
+
+#include "bits/stdc++.h"
+#include <algorithm>
+#include <climits>
+#include <codecvt>
+#include <map>
+#include <tuple>
+#include <vector>
+using namespace std;
+#define parr(x)                                                                 \
+   cerr << #x << ' ' << '[' << ' ';                                             \
+   for (auto z : x)                                                             \
+      cerr << z << ' ';                                                         \
+   cerr << ']' << endl
+#define pmap(x)                                                                 \
+   cerr << #x << ' ' << '{' << ' ';                                             \
+   for (auto z : x)                                                             \
+      cerr << '(' << ' ' << z.first << ' ' << z.second << ' ' << ')' << ' ';    \
+   cerr << '}' << endl
+#define fast                                                                    \
+   ios_base::sync_with_stdio(false);                                            \
+   cin.tie(NULL);                                                               \
+   cout.tie(NULL);                                                              \
+   cerr.tie(NULL);
+#undef int
+#define int long long int
+#define endl '\n'
+#define pi 3.1415926535
+int **memorized_dp_array;
+int no_of_vertix = 0;
+int **adj_matrix;
+int MOD = 1e9 + 7;
+map<int, vector<int>> tree;
+int *vis;
+char **grid;
+
+map<string, string> ma;
+
+void fun(string s,int l){
+   if(l == s.size()){
+      ma[s]=s;
+   }
+   for (int i = l; i < s.size(); i++) {
+      swap(s[l],s[i]);
+      fun(s, l+1);
+   }
+}
+
+
+void solution() {
+   string a;
+   cin >> a;
+   fun(a, 0);
+   std::cout << ma.size() << endl;
+   for (auto i : ma) {
+      std::cout << i.first << endl;
+   }
+}
+
+int32_t main() {
+   fast;
+   int test_cases = 1;
+   /* cin >> test_cases; */
+   /* int a = 1; */
+   while (test_cases--) {
+      /* std::cout << "Case #"<< a << ": "; */
+      solution();
+      /* a++; */
+   }
+   cerr << "=====" << endl;
+   cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+}
