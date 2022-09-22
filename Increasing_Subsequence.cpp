298091b7b68225d1cabd794bfce2f781c3aeef0b
Sharishth Singh <sharishthsingh@gmail.com>
2022-09-22 05:57:24 +0530

    Dynamic Programming 22 Sep

---
+++ Increasing_Subsequence.cpp
@@ -0,0 +1,89 @@
+/*
+ ______________________________________
+/           Title: cp template         \
+|        Author: Sharishth Singh       |
+\           Date: 22 May 2022          /
+ --------------------------------------
+        \   ^__^
+         \  (xx)\_______
+            (__)\       )\/\
+             U  ||----w||
+                ||     ||
+*/
+
+#include "bits/stdc++.h"
+#include <cerrno>
+#include <climits>
+#include <cmath>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <queue>
+#include <set>
+#include <utility>
+#include <vector>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+using namespace std;
+using namespace chrono;
+using namespace __gnu_pbds;
+typedef tree <pair<int, int>,null_type,less<pair<int, int>>,rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+#undef int
+#define int long long int
+#define endl '\n'
+#define INF 1000000000000
+#define pi 3.1415926535
+#define MOD 1000000007
+#define parr(x)                                                                 \
+   cerr << #x << ' ' << '[' << ' ';                                             \
+   for (auto z : x)                                                             \
+      cerr << z << ' ';                                                         \
+   cerr << ']' << endl
+#define pmap(x)                                                                 \
+   cerr << #x << ' ' << '{' << ' ';                                             \
+   for (auto z : x)                                                             \
+      cerr << '(' << ' ' << z.first << ' ' << z.second << ' ' << ')' << ' ';    \
+   cerr << '}' << endl
+#define fast                                                                    \
+   ios_base::sync_with_stdio(false);                                            \
+   cin.tie(0);                                                                  \
+   cout.tie(0);                                                                 \
+   cerr.tie(0);
+int **memorized_dp_array;
+int no_of_vertix = 0;
+int **adj_matrix;
+unordered_map<int,vector<pair<int,int>>> graph;
+int *vis;
+char **grid;
+
+void solution(){
+   int n;
+   cin >> n;
+   vector<int> dp;
+   for (int i = 0; i < n; i++) {
+      int x;
+      cin >> x;
+      auto it = lower_bound(dp.begin(), dp.end(), x);
+      if (it == dp.end()) {
+         dp.push_back(x);
+      } else {
+         *it = x;
+      }
+   }
+   cout << dp.size() << endl;
+}
+
+int32_t main() {
+   fast;
+   int test_cases = 1;
+   /* cin >> test_cases; */
+   /* int a = 1; */
+   while (test_cases--) {
+      /* std::cout << "Case #"<< a << ": "; */
+      solution();
+      /* a++; */
+   }
+   cerr << "=====" << endl;
+   cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+   return 0;
+}
+
